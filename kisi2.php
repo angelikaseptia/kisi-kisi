97b9bbe154be67dedb95d28e53d37598d9b7f5cd
angelikaseptia <angelikaseptia362@gmail.com>
2023-03-06 22:50:17 -0800

    kisi-kisi

---
+++ kisi2.php
@@ -0,0 +1,12 @@
+<?php 
+$nasabah =" kelas 2 ";
+if ($nasabah== " VIP "){
+    echo " kartu atm, buku tabungan, M-Baking, menjadi prioritas";
+}
+else if($nasabah==" Kelas 1 "){
+    echo " kartu atm, M-Baking, buku tabungan";
+} 
+else if ($nasabah==" kelas 2 "){
+    echo " kartu atm, buku tabungan";
+}
+?>
